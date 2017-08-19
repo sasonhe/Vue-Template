e52feef57619b05d8b3597a678f22d244f5c1e37
sasonhe <642139009@qq.com>
2017-08-19 16:18:01 +0800

    vue 模块化开发模板

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# my-test
-test
+# vue模块化开发模板
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm start
+
+# build for production with minification
+npm webpack
---
+++ src/index.js
@@ -0,0 +1,23 @@
+"use strict"
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import App from './App.vue'
+import routes from './router.config'
+import reset from './common/reset.css'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+
+var router = new VueRouter({
+    routes
+})
+
+new Vue({
+    el : '#app',
+    router,
+    render : function(h){
+        return h(App)
+    }
+})
